6b3913fc2d9f05c2d294149798d7f7176581b557
Christophe Delord <cdelord.fr>
2024-03-04 21:33:06 +0100

    update build.lua

--- build.lua
+++ build.lua
@@ -19,28 +19,22 @@ http://cdelord.fr/ypp
 ]]
 
 local F = require "F"
+local sys = require "sys"
 
 help.name "ypp"
 help.description "$name"
 
-var "builddir" ".build"
+local target, args = target(arg)
+if #args > 0 then
+    F.error_without_stack_trace(args:unwords()..": unexpected arguments")
+end
+
+var "builddir" (".build"/(target and target.name))
 clean "$builddir"
 
 ---------------------------------------------------------------------
 section "Compilation"
 ---------------------------------------------------------------------
-
-local targets = F(require "sys".targets):map(F.partial(F.nth, "name"))
-local target, ext = nil, ""
-F(arg) : foreach(function(a)
-    if targets:elem(a) then
-        if target then F.error_without_stack_trace("multiple target definition", 2) end
-        target = a
-        if target:match"windows" then ext = ".exe" end
-    else
-        F.error_without_stack_trace(a..": unknown argument")
-    end
-end)
 
 local sources = {
     ls "src/*.lua",
@@ -58,10 +52,10 @@ rule "luaxc" {
 }
 
 local compile = {
-    build("$builddir/ypp"..ext) {
+    build("$builddir/ypp"..(target or sys.build).exe) {
         "luaxc",
         sources,
-        arg = target and {"-t", target},
+        arg = target and {"-t", target.name},
     },
     build "$builddir/ypp.lua"         { "luax", sources, args="-t lua" },
     build "$builddir/ypp-pandoc.lua"  { "luax", sources, args="-t pandoc" },
